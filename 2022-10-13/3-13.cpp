61f9988d819e2fbc1543b3db81560e820d3d2881
MercuryLS <111221528+MercuryLS@users.noreply.github.com>
2022-10-14 17:48:52 +0800

    Add files via upload

---
+++ 2022-10-13/3-13.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+
+int Fibonacci(int n)
+{
+	int m; 
+	if(n==1||n==2)
+	return 1;
+	else
+    m=Fibonacci(n-1)+Fibonacci(n-2);
+	return m;
+	
+}
+
+int main()
+{
+	cout<<"Please input the number of items in a sequence:"; 
+	int n;
+	cin>>n; 
+	cout<<"It's value is:"<<Fibonacci(n)<<endl;
+	return 0;
+ }
