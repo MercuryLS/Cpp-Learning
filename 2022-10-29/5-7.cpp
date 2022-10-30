5a116a3677831f0a32e54d28976be8e1d97f9428
MercuryLS <111221528+MercuryLS@users.noreply.github.com>
2022-10-30 18:12:51 +0800

    Add files via upload

---
+++ 2022-10-29/5-7.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+class cat
+{
+public:
+    static void getNumOfCats()
+    {
+        cout<<"The num of cats is:"<<numOfCats<<endl;
+    }
+private:
+    static int numOfCats;
+};
+
+int cat::numOfCats=0;
+
+int main()
+{
+    cat cattt;
+    cattt.getNumOfCats();
+    return 0;
+}
---
+++ 2022-10-29/xyz.h
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+ 
+class X
+{
+    public:
+        X():i(0)
+        {}
+        friend class Y;
+        friend class Z;
+        friend void h(X *p);
+        void show()
+        {
+            cout<<i<<endl;
+        }
+    private:
+        int i;
+};
+ 
+class Y
+{
+    public:
+        void g(X *p);
+ 
+};
+ 
+class Z
+{   
+    public:
+        void f(X *p);
+ 
+};
+ 
+void Y::g(X *p)
+{
+    p->i++;
+}
+ 
+void Z::f(X *p)
+{
+    p->i+=5;
+}
+ 
+void h(X *p)
+{
+    p->i+=10;
+}
